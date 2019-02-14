7db5e2cfbfe8ae0a1b8076a9ebf908c31d699318
Ro Flo <iroflood@msn.com>
2019-02-14 23:28:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
+def fizzbuzz(i)
+  if i % 3 == 0 && i % 5 == 0
+    puts "FizzBuzz"
+  end
+  elseif i % 3 == 0
+    puts "Fizz"
+  end
+  elseif i % 5 == 0 
+    puts "Buzz"
+  end
+  else i
+end
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
